05928001dc49b6319fcde7d73573787df084a572
Scott Kennedy <scottyzen@gmail.com>
2020-05-23 22:11:03 +0100

    Changed functions headers

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -37,6 +37,10 @@ exports.handler = function (event, context, callback) {
 
     callback(null, {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify('successful')
       });
 
