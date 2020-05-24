404c8d02c05584083668186b0658c262d412cd06
Scott Kennedy <scottyzen@gmail.com>
2020-05-24 11:39:54 +0100

    Changed functions headers

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -38,8 +38,9 @@ exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
         },
         body: JSON.stringify('successful')
       });
