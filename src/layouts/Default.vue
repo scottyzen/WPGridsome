565cd1e038ab6ff108c94fef36dee9a26ffbf4c6
Scott Kennedy <scottyzen@gmail.com>
2020-11-03 21:19:06 +0000

    Footer typo

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -11,14 +11,14 @@
     <div class="min-h-screen pt-32 pb-12">
       <slot />
     </div>
-    <footer class="font-light text-white bg-gray-900">
+    <footer class="py-4 text-gray-800 bg-gray-200">
       <p class="p-4 text-sm text-center">
-        © 2020 WPGrdisome.com by
+        © 2020 WPGridsome.com by
         <a
           href="https://scottyzen.com?ref=wpgridsome"
           target="_blank"
           rel="noopener"
-          class="text-orange-300"
+          class="text-indigo-600"
           >Scott Kennedy</a
         >
       </p>
