53eef413098bbf30db20e7e579b5a0bb16355f82
Scott Kennedy <scottyzen@gmail.com>
2020-11-03 21:14:39 +0000

    Linked to repo

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <div>
+    <div class="flex items-center">
       <ul class="main-menu">
-        <g-link class="inline-block py-8 mr-8" to="/">Home</g-link>
-        <g-link class="inline-block py-8 mr-8" to="/posts">Posts</g-link>
-        <g-link class="inline-block py-8" to="/mail">Mail</g-link>
+        <g-link class="inline-block py-8 mr-10" to="/">Home</g-link>
+        <g-link class="inline-block py-8 mr-10" to="/posts">Posts</g-link>
+        <g-link class="inline-block py-8 mr-10" to="/mail">Mail</g-link>
       </ul>
+      <a
+        class="py-2 px-4 flex bg-indigo-600 hover:bg-indigo-700 rounded text-white"
+        href="https://github.com/scottyzen/WPGridsome"
+        target="_blank"
+        >Repo <g-image class="ml-2" src="~/assets/images/github.svg"></g-image
+      ></a>
     </div>
   </div>
 </template>
