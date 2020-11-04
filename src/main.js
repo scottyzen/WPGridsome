50aa09dd921573550a9bcee9128ff2b18a25957c
Scott Kennedy <scottyzen@gmail.com>
2020-11-04 20:25:37 +0000

    Compressed svgs and replaced favicon

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  head.link.push({
+    rel: "icon",
+    href: "/favicon.svg"
+  });
 }
