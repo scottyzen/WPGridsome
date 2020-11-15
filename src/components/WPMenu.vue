6721d3c87042a1aab269d516c596474c651ed7b4
Scott Kennedy <scottyzen@gmail.com>
2020-11-15 19:58:02 +0000

    Add WPMenu component

---
+++ src/components/WPMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul v-for="{ node } in getNavItems" :key="node.id">
+      <li v-for="{ node } in node.menuItems.edges" :key="node.id">
+        <g-link :to="node.path">{{ node.label }}</g-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<static-query>
+query WordPressMenus{
+  menus {
+    edges {
+      node {
+        slug
+        id
+        menuItems {
+          edges {
+            node {
+              label
+              path
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+export default {
+  props: ["slug"],
+  computed: {
+    getNavItems() {
+      return this.$static.menus.edges.filter((edge) => {
+        return edge.node.slug == this.slug;
+      });
+    },
+  },
+};
+</script>
