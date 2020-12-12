4ab68cb32870b21dbd849e24a534eadf38e38f79
Scott Kennedy <scottyzen@gmail.com>
2020-12-12 19:56:31 +0000

    Confingured PWA

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,6 +16,43 @@ module.exports = {
       options: {
         shouldTimeTravel: true
       }
+    },
+    {
+        use: 'gridsome-plugin-pwa',
+        options: {
+            // Service Worker Options
+            disableServiceWorker: false,
+            serviceWorkerPath: 'service-worker.js',
+            cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+            disableTemplatedUrls: false,       // Optional
+
+            // Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
+            manifestPath: 'manifest.json',
+            title: 'WPGridsome',
+            startUrl: '/',
+            display: 'standalone',
+            statusBarStyle: 'default',
+            themeColor: '#666600',
+            backgroundColor: '#ffffff',
+            icon: '',
+            shortName: 'WPGridsome',            // Optional
+            description: 'Gridsome is awesome!',// Optional
+            categories: ['development'],        // Optional
+            lang: 'en-GB',                      // Optional
+            dir: 'auto',                        // Optional
+            maskableIcon: true,                 // Optional
+            gcmSenderId: undefined,             // Optional
+
+            // Standard Meta Tags
+            svgFavicon: 'favicon.svg',          // Optional. Requires favicon.ico fallback
+
+            // Microsoft Windows Meta Tags
+            msTileColor: '#666600',             // Optional
+
+            // Apple MacOS Meta Tags
+            appleMaskIcon: 'favicon.svg',       // Optional
+            appleMaskIconColor: '#666600',      // Optional
+        }
     }
   ]
 }
