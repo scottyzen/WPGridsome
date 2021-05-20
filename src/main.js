10d22043901551312bd22f161db870c9b249ac04
2Cubed-ie <admin@2cubed.ie>
2021-05-20 17:23:25 +0100

    fix build error

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-import VueFuse from "vue-fuse";
+// import VueFuse from "vue-fuse";
 import PageTitle from "~/components/PageTitle.vue";
 import DefaultLayout from "~/layouts/Default.vue";
 import { yoastHead } from "../src/mixins/yoastHead";
-import VueLazyLoad from "vue-lazyload";
+// import VueLazyLoad from "vue-lazyload";
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
   Vue.component("PageTitle", PageTitle);
 
-  Vue.use(VueFuse);
-  if (isClient) {
-    Vue.use(VueLazyLoad);
-  }
+  // Vue.use(VueFuse);
+  // if (isClient) {
+  //   Vue.use(VueLazyLoad);
+  // }
   Vue.mixin(yoastHead);
 
   head.link.push({
